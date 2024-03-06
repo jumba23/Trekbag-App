fcba374638cb7ad3d614ca0795d8d7461f6c5f9c
Goran SD <g.cvetic23@gmail.com>
2024-03-06 14:04:34 -0800

    Add initialItems and secondaryButtons constants

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,5 +1,24 @@
+import { initialItems } from "../lib/constants";
+
 const ItemList = () => {
-  return <div>ItemList</div>;
+  return (
+    <ul>
+      {initialItems.map((item) => (
+        <Item key={item.name} name={item.name} checked={item.checked} />
+      ))}
+    </ul>
+  );
 };
 
 export default ItemList;
+
+const Item = () => {
+  return (
+    <li className="item">
+      <label>
+        <input type="checkbox" />
+        good mood
+      </label>
+    </li>
+  );
+};
---
+++ src/lib/constants.js
@@ -0,0 +1,12 @@
+export const initialItems = [
+  { id: 1, name: "good mood", checked: true },
+  { id: 2, name: "passport", checked: false },
+  { id: 3, name: "phone charger", checked: false },
+];
+
+export const secondaryButtons = [
+  "Mark all as complete",
+  "Mark all as incomplete",
+  "Reset to initial",
+  "Remove all items",
+];
