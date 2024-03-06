b236981a1379f73c5ec485e602ba778e139dfb9a
Goran SD <g.cvetic23@gmail.com>
2024-03-06 14:14:33 -0800

    Add state hook and update Item component

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,24 +1,28 @@
+import { useState } from "react";
 import { initialItems } from "../lib/constants";
 
 const ItemList = () => {
+  const [items, setItems] = useState(initialItems);
+
   return (
     <ul>
-      {initialItems.map((item) => (
-        <Item key={item.name} name={item.name} checked={item.checked} />
-      ))}
+      {initialItems.map((item) => {
+        return <Item key={item.name} item={item} checked={item.checked} />;
+      })}
     </ul>
   );
 };
 
 export default ItemList;
 
-const Item = () => {
+const Item = ({ item }) => {
   return (
     <li className="item">
       <label>
         <input type="checkbox" />
-        good mood
+        {item.name}
       </label>
+      <button>❌</button>
     </li>
   );
 };
