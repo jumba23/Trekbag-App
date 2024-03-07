341c430165ed182f9d8a36445736130be73ebd39
Goran SD <g.cvetic23@gmail.com>
2024-03-06 23:10:27 -0800

    Add EmptyView component and update ItemList component

---
+++ src/components/EmptyView.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const EmptyView = () => {
+  return (
+    <section>
+      <h3>Empty Packing List</h3>
+      <p>
+        Start by adding some items you absolutely you {"don't"} want to forget
+      </p>
+    </section>
+  );
+};
+
+export default EmptyView;
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,6 +1,9 @@
+import EmptyView from "./EmptyView";
+
 const ItemList = ({ items, handleDeleteItem, handleToggleItem }) => {
   return (
-    <ul>
+    <ul className="item-list">
+      {items.length === 0 && <EmptyView />}
       {items.map((item) => {
         return (
           <Item
