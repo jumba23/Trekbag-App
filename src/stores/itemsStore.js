220b5d358042f5d452396b4c87563e0280f8d25e
Goran SD <g.cvetic23@gmail.com>
2024-03-08 08:57:21 -0800

    Add zustand and use-sync-external-store dependencies

---
+++ src/stores/itemsStore.js
@@ -0,0 +1,37 @@
+import { create } from "zustand";
+import { initialItems } from "../lib/constants";
+
+create((set) => ({
+  items: initialItems,
+  addItem: (newItemText) => {
+    const newItem = {
+      id: new Date().getTime(),
+      packed: false,
+      value: newItemText,
+    };
+    set((state) => ({ items: [...state.items, newItem] }));
+  },
+  deleteItem: (id) => {
+    const newItems = items.filter((item) => item.id !== id);
+    return { items: newItems };
+  },
+  removeAllItems: () => set({ items: [] }),
+  toggleItem: (id) =>
+    set((state) => {
+      const newItems = state.items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      );
+      return { items: newItems };
+    }),
+  resetToInitialItems: () => set({ items: initialItems }),
+  markAllAsComplete: () =>
+    set((state) => {
+      const newItems = state.items.map((item) => ({ ...item, packed: true }));
+      return { items: newItems };
+    }),
+  markAllAsIncomplete: () =>
+    set((state) => {
+      const newItems = state.items.map((item) => ({ ...item, packed: false }));
+      return { items: newItems };
+    }),
+}));
